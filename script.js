8e074683d50509863a56847c706fcf29531ab06d
wissal <wissal.ouarsafi@gmail.com>
2020-04-06 18:13:35 -0700

    Fetch posts from Api, Diplay them in the DOM

---
+++ script.js
@@ -0,0 +1,38 @@
+const postsContainer = document.getElementById('post-container');
+const loader = document.querySelector('.loader');
+const filter = document.getElementById('filter');
+
+let limit = 3;
+let page = 1;
+
+// Fetch posts from API
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+
+  const data = await res.json();
+
+  return data;
+}
+
+async function showPosts(){
+    const posts = await getPosts();
+    
+    posts.forEach(post => {
+        const postEl = document.createElement('div');
+        postEl.classList.add('post');
+
+        postEl.innerHTML = `
+        <div class="number">${post.id}</div>
+        <div class="post-info">
+          <h2 class="post-title">${post.title}</h2>
+          <p class="post-body">${post.body}</p>
+        </div>
+        `
+
+        postsContainer.appendChild(postEl);
+    })
+}
+
+showPosts()
